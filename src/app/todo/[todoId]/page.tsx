997c49f439f21a8e1830fdadc2821daf29953ea6
Kostjn04 <kostjn04e@gmail.com>
2025-08-24 21:47:24 +0300

    fix todo page props

--- src/app/todo/[todoId]/page.tsx
+++ src/app/todo/[todoId]/page.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import TaskPage from '../../../components/task/TaskPage';
 import UIContainer from '../../../components/ui/uiContainer/UIContainer';
 
-const TodoPage = async({params}: {params: {todoId: string}}) => {
+interface TodoPageProps {
+  params: Promise<{todoId: string}>;
+}
+
+const TodoPage = async({params}: TodoPageProps) => {
   const {todoId} = await params;
   return (
     <UIContainer>
